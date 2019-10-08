a48059d378f34fc73f6c3a2febca5e7dbdfb0454
Ester <ester.caroline.1009@gmail.com>
2019-10-07 22:33:14 -0300

    exercicio 6

--- app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements CustomDialog.ItemListener{
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemLongClickListener,
+        CustomDialog.ItemListener{
 
     private boolean insertMode;
     private ItemAdapter adapter;
@@ -56,4 +60,14 @@ public class MainActivity extends AppCompatActivity implements CustomDialog.Item
         }
     }
 
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+     PopupMenu popup = new PopupMenu(this,view);
+     popup.inflate(R.menu.popup);
+     popup.show();
+
+
+        return true;
+    }
 }
